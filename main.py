d4da84de1b149e79bb26bfad9d4e0694bd5f65cc
Samiran-Chakraborty <84717651+Samiran-Chakraborty@users.noreply.github.com>
2026-06-15 18:36:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.responses import FileResponse
-
+import os
+from reportlab.pdfgen import canvas
 
 app = FastAPI()
 
+
 @app.post("/api/v1/deliverables/generate")
 def generate():
-    
-    # ✅ Simulate file generation
-    file_path = "sample.pdf"
+
+    file_path = "output.pdf"
+
+    # ✅ Create PDF dynamically
+    c = canvas.Canvas(file_path)
+    c.drawString(100, 750, "Dynamic Document")
+    c.drawString(100, 720, "Generated at runtime ✅")
+    c.drawString(100, 690, "Project Document Example")
+    c.save()
 
     return {
         "status": "SUCCESS",
-        "message": "Document generated successfully",
+        "message": "Document generated",
         "file_url": "https://python-document-new-2.onrender.com/api/v1/deliverables/download"
     }
 
+
 @app.get("/api/v1/deliverables/download")
 def download():
+
+    file_path = os.path.join(os.getcwd(), "output.pdf")
+
     return FileResponse(
-        path="sample.pdf",
+        path=file_path,
         filename="deliverable.pdf",
         media_type="application/pdf"
     )
